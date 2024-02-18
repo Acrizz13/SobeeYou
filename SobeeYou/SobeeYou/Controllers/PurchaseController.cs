7d374c989f8792375ccf87a85ef6ef5e9a678735
Aleksander Chrismer <achrismer@cincinnatistate.edu>
2024-02-18 09:06:38 -0500

    Examples

---
+++ SobeeYou/SobeeYou/Controllers/PurchaseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SobeeYou.Controllers
+{
+    public class PurchaseController : Controller
+    {
+        // GET: Purchase
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
