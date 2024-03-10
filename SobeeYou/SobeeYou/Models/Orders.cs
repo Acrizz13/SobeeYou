1afa2b7c49578a2e207319efd31b75255d241901
e-million <eyassu.million@gmail.com>
2024-03-09 16:50:48 -0800

    added UserOrders view, updated Orders model

--- SobeeYou/SobeeYou/Models/Orders.cs
+++ SobeeYou/SobeeYou/Models/Orders.cs
@@ -6,9 +6,13 @@ using System.Web;
 namespace SobeeYou.Models {
     public class Orders {
         public int intOrderID { get; set; }
-        public string intUserID { get; set; }
-        public DateTime dtmDateAdded { get; set; }
+        public int intUserID { get; set; }
+        public DateTime dtmOrderDate { get; set; }
         public decimal decTotalAmount { get; set; }
+        public int intShippingStatusID { get; set; }
+        public string strShippingAddress { get; set; }
+        public string strTrackingNumber { get; set; }
+        public int intPaymentMethod { get; set; }
 
 
     }
