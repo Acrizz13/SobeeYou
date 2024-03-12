0fc0e5869742ba0ed20f5ba16fb21f5133cf6b0e
e-million <eyassu.million@gmail.com>
2024-03-12 01:50:54 -0700

    replaced all userModel stuff with new TUser model

---
+++ SobeeYou/SobeeYou/Models/ProductDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SobeeYou.Models {
+    public class ProductDTO {
+        public int intProductID { get; set; }
+        public string strName { get; set; }
+        public decimal decPrice { get; set; }
+        public string strStockAmount { get; set; }
+    }
+}
