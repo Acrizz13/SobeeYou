3a633d99b02ce206e348e7a73c28c8f6649f79d8
e-million <eyassu.million@gmail.com>
2024-04-08 20:01:08 -0400

    added josh's code from old project, added add to cart functionality as well to the page

--- SobeeYou/sobee_core/sobee_core/Controllers/PurchaseController.cs
+++ SobeeYou/sobee_core/sobee_core/Controllers/PurchaseController.cs
@@ -8,36 +8,64 @@ using sobee_core.Models.AzureModels.Identity;
 
 
 namespace sobee_core.Controllers {
-    public class PurchaseController : Controller {
+	public class PurchaseController : Controller {
 
-        // GET: Purchase
+		// GET: Purchase
 
-        private readonly SobeecoredbContext _context;
-        private readonly UserManager<ApplicationUser> _userManager;
-        private int shoppingCartID;
-
-
-        public PurchaseController(SobeecoredbContext context, UserManager<ApplicationUser> userManager) {
-            _context = context;
-            _userManager = userManager;
-        }
+		private readonly SobeecoredbContext _context;
+		private readonly UserManager<ApplicationUser> _userManager;
+		private int shoppingCartID;
 
 
-        public IActionResult Index() {
-            // Retrieve the required product information using LINQ to Entities
-            var products = _context.Tproducts
-                .Select(p => new ProductDTO {
-                    // Map the properties from the TProduct entity to the ProductDTO
-                    intProductID = p.IntProductId,
-                    strName = p.StrName,
-                    decPrice = (decimal)p.DecPrice,
-                    strStockAmount = p.StrStockAmount
-                })
-                .ToList(); // Execute the query and convert the result to a list
+		public PurchaseController(SobeecoredbContext context, UserManager<ApplicationUser> userManager) {
+			_context = context;
+			_userManager = userManager;
+		}
 
-            // Pass the retrieved products to the view
-            return View(products);
-        }
 
-    }
+		public IActionResult Index() {
+			// Retrieve the required product information using LINQ to Entities
+			var products = _context.Tproducts
+				.Select(p => new ProductDTO {
+					// Map the properties from the TProduct entity to the ProductDTO
+					intProductID = p.IntProductId,
+					strName = p.StrName,
+					decPrice = (decimal)p.DecPrice,
+					strStockAmount = p.StrStockAmount
+				})
+				.ToList(); // Execute the query and convert the result to a list
+
+			// Pass the retrieved products to the view
+			return View(products);
+		}
+
+		// shows detailed view of products
+		public ActionResult Details(int id) {
+
+			var product = _context.Tproducts
+				.Where(p => p.IntProductId == id)
+				.Select(p => new ProductDTO {
+					intProductID = p.IntProductId,
+					strName = p.StrName,
+					decPrice = (decimal)p.DecPrice,
+					strStockAmount = p.StrStockAmount,
+					// Add additional properties as needed
+				})
+				.FirstOrDefault();
+
+			//if (product == null) {
+			//	return HttpNotFound();
+			//}
+
+			return View(product);
+		}
+
+
+
+	}
+
+
+
+
+
 }
