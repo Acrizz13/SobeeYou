887ebdf50b6c50d64cbd2e0694047e16f7ebd905
e-million <eyassu.million@gmail.com>
2024-04-23 15:02:03 -0400

    added pages to navbar: - FAQ - Contact - FindUs
    added to footer:
    - Refund Policy
    - Terms of Service
    - Shipping Returns
    - all new links from navbar
    
    moved heat map from admin dashboard to its own view.
    
    all new views are in the about folder and use the about controller

--- SobeeYou/sobee_core/sobee_core/Controllers/AboutController.cs
+++ SobeeYou/sobee_core/sobee_core/Controllers/AboutController.cs
@@ -1,10 +1,86 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
 
 namespace sobee_core.Controllers {
-    public class AboutController : Controller {
+	public class AboutController : Controller {
 
-        public IActionResult Index() {
-            return View();      // returns view
-        }
-    }
+		public IActionResult Index() {
+			return View();      // returns view
+		}
+
+
+
+		public IActionResult FAQ() {
+
+			return View();
+		}
+
+		public IActionResult RefundPolicy() {
+			return View();
+		}
+
+		public IActionResult ShippingReturns() {
+
+			return View();
+
+		}
+
+		public IActionResult TermsOfService() {
+			return View();
+		}
+
+
+		public IActionResult FindUs() {
+			return View();
+		}
+
+		public IActionResult Contact() {
+			return View();
+		}
+
+
+		[HttpPost]
+		public ActionResult Contact(string email, string message) {
+			// TODO: Implement email sending functionality
+			// Send email using the provided email and message
+			string workEmail = "sobeeyoubusiness@gmail.com"; // replace with sobee email
+			string fromPassword = "yplu kfwq wufa jpjp"; // replace with sobee app password
+
+			SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+			smtpClient.EnableSsl = true;
+			smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+			smtpClient.UseDefaultCredentials = false;
+			smtpClient.Credentials = new NetworkCredential(workEmail, fromPassword);
+
+			// Create the password reset email
+			MailMessage mailMessage = new MailMessage();
+			mailMessage.From = new MailAddress(workEmail);
+			mailMessage.To.Add(workEmail);
+			mailMessage.Subject = "New Inquiry from Customer!";
+			mailMessage.Body = "Here is a new message from " + email + ": " + message;
+
+			// Send the email
+			smtpClient.Send(mailMessage);
+			TempData["SuccessMessage"] = "Your message to SoBee You has been sent! Please check your email for a reply within 1-2 business days.";
+			return RedirectToAction("Contact");
+		}
+
+
+
+
+
+
+
+
+
+
+
+	}
 }
+
+// FAQ
+// Shipping and Returns
+// Contact
+//Refund Policy
+// Terms of service
