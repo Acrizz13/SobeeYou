ae3c383bed8080fe5e1a1b1d70d00f18c753852f
e-million <eyassu.million@gmail.com>
2024-04-29 00:11:49 -0400

    added review reply models and to db context

--- SobeeYou/sobee_core/sobee_core/Models/AzureModels/Treview.cs
+++ SobeeYou/sobee_core/sobee_core/Models/AzureModels/Treview.cs
@@ -6,22 +6,25 @@ using sobee_core.Models.Identity;
 namespace sobee_core.Models.AzureModels;
 
 public partial class Treview {
-    [Key]
-    public int IntReviewId { get; set; }
+	[Key]
+	public int IntReviewId { get; set; }
 
-    public int IntProductId { get; set; }
+	public int IntProductId { get; set; }
 
-    public string StrReviewText { get; set; } = null!;
+	public string StrReviewText { get; set; } = null!;
 
-    public int IntRating { get; set; }
+	public int IntRating { get; set; }
 
-    public DateTime DtmReviewDate { get; set; }
+	public DateTime DtmReviewDate { get; set; }
 
-    public string? UserId { get; set; }
+	public string? UserId { get; set; }
 
-    public string? SessionId { get; set; }
+	public string? SessionId { get; set; }
 
-    public virtual Tproduct IntProduct { get; set; } = null!;
+	public virtual Tproduct IntProduct { get; set; } = null!;
 
-    public virtual AspNetUser? User { get; set; }
+	public virtual ICollection<TReviewReplies> TReviewReplies { get; set; } = new List<TReviewReplies>();
+
+	public virtual AspNetUser? User { get; set; }
+
 }
